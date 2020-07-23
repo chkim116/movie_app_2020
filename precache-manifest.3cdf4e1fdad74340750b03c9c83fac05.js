22c292ddf3b59b76a7d0a12a394c9651aa850e3f
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-23 21:38:58 +0900

    Updates

---
+++ precache-manifest.3cdf4e1fdad74340750b03c9c83fac05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be30f93099af74162a458d490f94cdc",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a19be443033c3792a5a5",
+    "url": "/movie_app_2020/static/css/main.3d385782.chunk.css"
+  },
+  {
+    "revision": "4dc02c729119b458c529",
+    "url": "/movie_app_2020/static/js/2.9e6c1f20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.9e6c1f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19be443033c3792a5a5",
+    "url": "/movie_app_2020/static/js/main.4f3efafb.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
