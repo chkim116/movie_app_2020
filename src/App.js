21b5ad280ec5a3b1dfb3647d30ac4b974409c500
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-23 14:37:59 +0900

    리액트 연습중
    class, state, function, map

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
 
-function Food({fav}) {
-return <h3>Hello! I LUV {fav}</h3>
-}
-
-function App() {
-  return (
-  <div>
-    
-    <Food fav="kimchis" />
-    <h1>hi</h1>
-    <Food fav="donggas" />
-  </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  pow = () => {
+    this.setState((current) => ({ count: current.count * current.count }));
+  };
+  reset = () => {
+    this.setState({ count: 0 });
+  };
+  render() {
+    return (
+      <div>
+        <h3>The number is : {this.state.count}</h3>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+        <button onClick={this.pow}>power</button>
+        <button onClick={this.reset}>reset</button>
+      </div>
+    );
   }
-
+}
+// this는 상위에 있는 선언한 객체를 불러온다고 생각하면된다. 여기서는 App을 뜻한다.
+// 그래서 App.state.count라고 생각하면 된다.
 // div안에 있어야 한다. (태그안에)
 export default App;
-
